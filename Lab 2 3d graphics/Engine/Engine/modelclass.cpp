b33e05f80ab425f16083978e75b609a4ca0f033b
Viktor <ctxx61@gmail.com>
2017-09-13 10:35:47 +0100

    Draw square, Star, circle

--- Lab 2 3d graphics/Engine/Engine/modelclass.cpp
+++ Lab 2 3d graphics/Engine/Engine/modelclass.cpp
@@ -69,12 +69,14 @@ bool ModelClass::InitializeBuffers(ID3D11Device* device)
     D3D11_SUBRESOURCE_DATA vertexData, indexData;
 	HRESULT result;
 
-
+	int m_slides = 32;
+	float radius = 2.0f;
+	double angle = 360 / m_slides;
 	// Set the number of vertices in the vertex array.
-	m_vertexCount = 3;
+	m_vertexCount = m_slides+1;
 
 	// Set the number of indices in the index array.
-	m_indexCount = 3;
+	m_indexCount = m_slides*3;
 
 	// Create the vertex array.
 	vertices = new VertexType[m_vertexCount];
@@ -91,19 +93,148 @@ bool ModelClass::InitializeBuffers(ID3D11Device* device)
 	}
 
 	// Load the vertex array with data.
-	vertices[0].position = D3DXVECTOR3(-1.0f, -1.0f, 0.0f);  // Bottom left.
-	vertices[0].color = D3DXVECTOR4(0.0f, 1.0f, 0.0f, 1.0f);
 
-	vertices[1].position = D3DXVECTOR3(0.0f, 1.0f, 0.0f);  // Top middle.
-	vertices[1].color = D3DXVECTOR4(0.0f, 1.0f, 0.0f, 1.0f);
+	//Triangle
+	//vertices[0].position = D3DXVECTOR3(-1.0f, -1.0f, 0.0f);  // Bottom left.
+	//vertices[0].color = D3DXVECTOR4(0.0f, 1.0f, 0.0f, 1.0f);
 
-	vertices[2].position = D3DXVECTOR3(1.0f, -1.0f, 0.0f);  // Bottom right.
-	vertices[2].color = D3DXVECTOR4(0.0f, 1.0f, 0.0f, 1.0f);
+	//vertices[1].position = D3DXVECTOR3(0.0f, 1.0f, 0.0f);  // Top middle.
+	//vertices[1].color = D3DXVECTOR4(0.0f, 1.0f, 0.0f, 1.0f);
+
+	//vertices[2].position = D3DXVECTOR3(1.0f, -1.0f, 0.0f);  // Bottom right.
+	//vertices[2].color = D3DXVECTOR4(0.0f, 1.0f, 0.0f, 1.0f);
+
+	//Square
+	//vertices[0].position = D3DXVECTOR3(0.0f, 1.0f, 0.0f);  // Bottom left.
+	//vertices[0].color = D3DXVECTOR4(0.0f, 1.0f, 0.0f, 1.0f);
+
+	//vertices[1].position = D3DXVECTOR3(1.0f, 1.0f, 0.0f);  // Top middle.
+	//vertices[1].color = D3DXVECTOR4(0.0f, 1.0f, 0.0f, 1.0f);
+
+	//vertices[2].position = D3DXVECTOR3(1.0f, 0.0f, 0.0f);  // Bottom right.
+	//vertices[2].color = D3DXVECTOR4(0.0f, 1.0f, 0.0f, 1.0f);
+
+
+	//vertices[3].position = D3DXVECTOR3(0.0f, 0.0f, 0.0f);  // Bottom right.
+	//vertices[3].color = D3DXVECTOR4(0.0f, 1.0f, 0.0f, 1.0f);
+
+	//Star
+	//vertices[0].position = D3DXVECTOR3(0.0f, 1.0f, 0.0f);  // Bottom left.
+	//vertices[0].color = D3DXVECTOR4(0.0f, 1.0f, 0.0f, 1.0f);
+
+	//vertices[1].position = D3DXVECTOR3(0.224f, 0.309f, 0.0f);  // Top middle.
+	//vertices[1].color = D3DXVECTOR4(0.0f, 1.0f, 0.0f, 1.0f);
+
+	//vertices[2].position = D3DXVECTOR3(0.951f, 0.309f, 0.0f);  // Bottom right.
+	//vertices[2].color = D3DXVECTOR4(0.0f, 1.0f, 0.0f, 1.0f);
+
+	//vertices[3].position = D3DXVECTOR3(0.363f, -0.118f, 0.0f);  // Bottom right.
+	//vertices[3].color = D3DXVECTOR4(0.0f, 1.0f, 0.0f, 1.0f);
+
+	//vertices[4].position = D3DXVECTOR3(0.587f, -0.809f, 0.0f);  // Bottom right.
+	//vertices[4].color = D3DXVECTOR4(0.0f, 1.0f, 0.0f, 1.0f);
+
+	//vertices[5].position = D3DXVECTOR3(0.0f, -0.382f, 0.0f);  // Bottom right.
+	//vertices[5].color = D3DXVECTOR4(0.0f, 1.0f, 0.0f, 1.0f);
+
+	//vertices[6].position = D3DXVECTOR3(-0.588f, -0.809f, 0.0f);  // Bottom right.
+	//vertices[6].color = D3DXVECTOR4(0.0f, 1.0f, 0.0f, 1.0f);
+
+	//vertices[7].position = D3DXVECTOR3(-0.363f, -0.118f, 0.0f);  // Bottom right.
+	//vertices[7].color = D3DXVECTOR4(0.0f, 1.0f, 0.0f, 1.0f);
+
+	//vertices[8].position = D3DXVECTOR3(-0.951f, 0.309f, 0.0f);  // Bottom right.
+	//vertices[8].color = D3DXVECTOR4(0.0f, 1.0f, 0.0f, 1.0f);
+
+	//vertices[9].position = D3DXVECTOR3(-0.224f, 0.309f, 0.0f);  // Bottom right.
+	//vertices[9].color = D3DXVECTOR4(0.0f, 1.0f, 0.0f, 1.0f);
+
+	//Circle
+	vertices[0].position = D3DXVECTOR3(0.0f, 0.0f, 0.0f);  
+	vertices[0].color = D3DXVECTOR4(1.0f, 0.0f, 0.0f, 1.0f);  //Red, Green, Blue
+	float tempColor = 1.0f / (float)m_slides;
+	float colorX, colorY, colorZ;
+	colorX = 0.0f;
+	colorY = 0.0f;
+	colorZ = 0.0f;
+	for (int i = 0; i < m_vertexCount-1; i++) 
+	{
+		float tempAngle = i * angle;
+		float X = radius * cos(tempAngle*PI / 180);
+		float Y = radius * sin(tempAngle*PI / 180);
+		if (tempAngle == 90)
+		{
+			X = 0;
+		}
+
+		if (tempAngle == 270)
+		{
+			X = 0;
+		}
+		if (tempAngle == 0)
+		{
+			Y = 0;
+		}
+		if (tempAngle == 180)
+		{
+			Y = 0;
+		}
+		
+		colorX = colorX + 2 * tempColor;
+		colorZ = colorZ + 2 * tempColor;
+		vertices[i+1].position = D3DXVECTOR3(X, Y, 0.0f);
+		vertices[i+1].color = D3DXVECTOR4(1.0f, 0.0f, 0.0f, 1.0f);
+	}
 
 	// Load the index array with data.
-	indices[0] = 0;  // Bottom left.
-	indices[1] = 1;  // Top middle.
-	indices[2] = 2;  // Bottom right.
+
+	//Triangle
+	//indices[0] = 1;  // Bottom left.
+	//indices[1] = 0;  // Top middle.
+	//indices[2] = 2;  // Bottom right.
+
+	////Star
+	//indices[0] = 9;  // Bottom left.
+	//indices[1] = 0;  // Top middle.
+	//indices[2] = 1;  // Bottom right.
+	//indices[3] = 9;
+	//indices[4] = 1;
+	//indices[5] = 3;
+	//indices[6] = 1;  
+	//indices[7] = 2;  
+	//indices[8] = 3;  
+	//indices[9] = 3;
+	//indices[10] = 4;
+	//indices[11] = 5;
+	//indices[12] = 3;  
+	//indices[13] = 5;  
+	//indices[14] = 7;  
+	//indices[15] = 5;
+	//indices[16] = 6;
+	//indices[17] = 7;
+	//indices[18] = 7;
+	//indices[19] = 9;
+	//indices[20] = 3;
+	//indices[21] = 7;
+	//indices[22] = 8;
+	//indices[23] = 9;
+
+	//circle
+	int offset = 1;
+	indices[0] = 1;
+	for (int i = 1; i < m_indexCount-2; i++)
+	{
+		int temp = i % 3;
+		if(temp == 1)
+		{
+			indices[i] = 0;
+			indices[i + 1] = offset + 1;
+			indices[i + 2] = offset + 1;
+			offset++;
+		}
+	}
+	indices[m_indexCount - 1] = 1;
+
 
 	// Set up the description of the static vertex buffer.
     vertexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
--- Lab 2 3d graphics/Engine/Engine/systemclass.cpp
+++ Lab 2 3d graphics/Engine/Engine/systemclass.cpp
@@ -245,7 +245,7 @@ void SystemClass::InitializeWindows(int& screenWidth, int& screenHeight)
 
 	// Create the window with the screen settings and get the handle to it.
 	m_hwnd = CreateWindowEx(WS_EX_APPWINDOW, m_applicationName, m_applicationName, 
-						    WS_CLIPSIBLINGS | WS_CLIPCHILDREN | WS_POPUP,
+						    WS_CLIPSIBLINGS | WS_CLIPCHILDREN | WS_POPUP | WS_OVERLAPPEDWINDOW,
 						    posX, posY, screenWidth, screenHeight, NULL, NULL, m_hinstance, NULL);
 
 	// Bring the window up on the screen and set it as main focus.
@@ -254,7 +254,7 @@ void SystemClass::InitializeWindows(int& screenWidth, int& screenHeight)
 	SetFocus(m_hwnd);
 
 	// Hide the mouse cursor.
-	ShowCursor(false);
+	ShowCursor(true);
 
 	return;
 }
