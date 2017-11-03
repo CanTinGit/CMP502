2e51e1b6252903e479ced2b6e04c66d6a607d222
Viktor <ctxx61@gmail.com>
2017-11-03 13:14:44 +0000

    add input function

--- Lab 4 Model loading and shaders/Engine/Engine/graphicsclass.cpp
+++ Lab 4 Model loading and shaders/Engine/Engine/graphicsclass.cpp
@@ -92,11 +92,11 @@ bool GraphicsClass::Initialize(int screenWidth, int screenHeight, HWND hwnd)
 	}
 
 	// Initialize the light object.
-	m_Light->SetAmbientColor(0.0f, 0.5f, 0.0f, 1.0f);
-	m_Light->SetDiffuseColor(0.0f, 0.0f, 0.5f, 1.0f);
+	m_Light->SetAmbientColor(0.1f, 0.1f, 0.1f, 1.0f);
+	m_Light->SetDiffuseColor(0.5f, 0.5f, 0.5f, 1.0f);
 	m_Light->SetDirection(0.5f, 0.0f, 0.8f);
-	m_Light->SetSpecularColor(0.5f, 0.0f, 0.0f, 1.0f);
-	m_Light->SetSpecularPower(2.0f);
+	m_Light->SetSpecularColor(1.0f, 1.0f, 1.0f, 1.0f);
+	m_Light->SetSpecularPower(32.0f);
 	
 
 	return true;
--- Lab 4 Model loading and shaders/Engine/Engine/systemclass.cpp
+++ Lab 4 Model loading and shaders/Engine/Engine/systemclass.cpp
@@ -42,8 +42,12 @@ bool SystemClass::Initialize()
 	}
 
 	// Initialize the input object.
-	m_Input->Initialize();
-
+	result = m_Input->Initialize(m_hinstance, m_hwnd, screenWidth, screenHeight);
+	if (!result) 
+	{
+		MessageBox(m_hwnd, L"Could not initialize the input object.", L"Error", MB_OK);
+		return false;
+	}
 	// Create the graphics object.  This object will handle rendering all the graphics for this application.
 	m_Graphics = new GraphicsClass;
 	if(!m_Graphics)
@@ -73,8 +77,9 @@ void SystemClass::Shutdown()
 	}
 
 	// Release the input object.
-	if(m_Input)
+	if (m_Input)
 	{
+		m_Input->Shutdown();
 		delete m_Input;
 		m_Input = 0;
 	}
@@ -120,7 +125,10 @@ void SystemClass::Run()
 				done = true;
 			}
 		}
-
+		if (m_Input->IsEscapePressed() == true)
+		{
+			done = true;
+		}
 	}
 
 	return;
@@ -133,7 +141,8 @@ bool SystemClass::Frame()
 
 
 	// Check if the user pressed escape and wants to exit the application.
-	if(m_Input->IsKeyDown(VK_ESCAPE))
+	result = m_Input->Frame();
+	if (!result)
 	{
 		return false;
 	}
@@ -151,30 +160,7 @@ bool SystemClass::Frame()
 
 LRESULT CALLBACK SystemClass::MessageHandler(HWND hwnd, UINT umsg, WPARAM wparam, LPARAM lparam)
 {
-	switch(umsg)
-	{
-		// Check if a key has been pressed on the keyboard.
-		case WM_KEYDOWN:
-		{
-			// If a key is pressed send it to the input object so it can record that state.
-			m_Input->KeyDown((unsigned int)wparam);
-			return 0;
-		}
-
-		// Check if a key has been released on the keyboard.
-		case WM_KEYUP:
-		{
-			// If a key is released then send it to the input object so it can unset the state for that key.
-			m_Input->KeyUp((unsigned int)wparam);
-			return 0;
-		}
-
-		// Any other messages send to the default message handler as our application won't make use of them.
-		default:
-		{
-			return DefWindowProc(hwnd, umsg, wparam, lparam);
-		}
-	}
+	return DefWindowProc(hwnd, umsg, wparam, lparam);
 }
 
 
